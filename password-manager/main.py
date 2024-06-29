1a5e0ec2f44d81ce16ca1769cd69598ccba5bd7f
cochecheee <buitien747@gmail.com>
2024-06-29 16:20:44 +0700

    maanage data through json file

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,10 +1,31 @@
 from tkinter import *
 from tkinter import messagebox
 import randompass
+import json
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20,pady=20)
+window.config(padx=50, pady=50)
+
+# ------------------------------------- SEARCH FOR A WEBSITE
+def search():
+    # take the name of website
+    website = website_input.get()
+
+    # get data from json file
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showwarning(title="Oops", message="Data File Not Found")
+    else:
+        if website in data.keys():
+            email = data[website]["name"]
+            password = data[website]["password"]
+            message=f"Email: {email}\nPassword: {password}"
+            messagebox.showinfo(title=website,message=message)
+        else:
+            messagebox.showinfo(title=website,message=f"No account was found for {website}")
 
 # ------------------------------------- GENERATE A PASSWORD
 def generate_password():
@@ -14,9 +35,31 @@ def generate_password():
 
 # ------------------------------------- SAVE DATA TO FILE
 def save_file(website,username,password):
-    with open("data.txt",mode="a") as file:
-        data = f"{website} | {username} | {password}\n"
-        file.writelines(data)
+    # with open("data.txt",mode="a") as file:
+    #     data = f"{website} | {username} | {password}\n"
+    #     file.writelines(data)
+    new_data = {
+        website : {
+            "name" : username,
+            "password" : password,
+        }
+    }
+
+    # load data 
+    try:
+        with open("data.json","r") as data_file:
+            # read old data
+            data = json.load(data_file)
+    except:
+        data = {}
+        data.update(new_data)
+    
+    # update and write data
+    with open("data.json","w") as data_file:
+        # update new data to old data
+        data.update(new_data)
+        # write to file
+        json.dump(data,data_file,indent=4)
     
 def add_account():
     website = website_input.get()
@@ -63,6 +106,7 @@ website_input = Entry(width=35)
 username_input = Entry(width=35)
 pass_input = Entry(width=21)
 website_input.focus()
+username_input.insert(0,"coche@coche.vn")
 website_input.grid(row=1,column=1,columnspan=2)
 username_input.grid(row=2,column=1,columnspan=2)
 pass_input.grid(row=3,column=1)
@@ -72,5 +116,9 @@ gen_pass_btn = Button(text="Generate Password", command=generate_password)
 add_btn = Button(text="Add",width=36,command=add_account)
 gen_pass_btn.grid(row=3,column=2)
 add_btn.grid(row=4, column=1, columnspan=2)
+# # # new search button
+search_btn = Button(text="Search",padx=10,command=search)
+search_btn.grid(row=1, column=2)
 
 window.mainloop()
+
