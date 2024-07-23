ddbfc6f1220728f585384b494078ac309ede4bb6
cochecheee <buitien747@gmail.com>
2024-07-23 22:48:40 +0700

    build route for login

--- html-form/main.py
+++ html-form/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+# Flask has a method called request (don't confuse this with the requests module)
+from flask import request as req
 
 app = Flask(__name__)
 
@@ -6,5 +8,12 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# CHALLENGE: See if you can use the documentation below to figure out how to make our HTML form submit a "POST" request to the path "/login".
+@app.route('/login',methods=["POST"])
+def login():
+    if req.method == 'POST':
+        return "💪 Success! Form submitted"
+    else:
+        return "<h1>Fail to make POST request to server</h1>"
 if __name__ == '__main__':
     app.run(debug=True)
